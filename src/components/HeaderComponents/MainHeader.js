77bb12650cc7f3ddd0361d51f8b0da3b7436b91d
Priyesh <priyeshraut5@gmail.com>
2024-07-10 22:02:06 +0530

    update header

--- src/components/HeaderComponents/MainHeader.js
+++ src/components/HeaderComponents/MainHeader.js
@@ -1,16 +1,45 @@
-import { Link } from "react-router-dom";
-import useMainHeader from "../../hooks/useMainHeader";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { productData } from "../../utils/productData";
+import { setFilteredProducts } from "../../utils/searchSlice";
+import { signOut } from "firebase/auth";
+import { auth } from "../../utils/firebase";
 
 const MainHeader = ({ showAside }) => {
+  const [query, setQuery] = useState("");
+  const navigate = useNavigate();
+  const dispath = useDispatch();
 
-  const {
-    handleSearch,
-    query,
-    setQuery,
-    user,
-    handleSignOut,
-    totalCartItems
-  } = useMainHeader();
+  const user = useSelector((store) => store.user);
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+
+    const searchFilterItenNameWithItemCategroy = productData.filter((data) =>
+      (data.category + data.itemName)
+        .toString()
+        .toLowerCase()
+        .includes(query.toLowerCase())
+    );
+
+    dispath(setFilteredProducts(searchFilterItenNameWithItemCategroy));
+
+    navigate(`/search?query=${encodeURIComponent(query)}`);
+  };
+
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        navigate("/login");
+      })
+      .catch(() => {
+        navigate("/error");
+      });
+  };
+
+  const cartItems = useSelector((store) => store.cart.items);
+  const totalCartItems = cartItems.reduce((total, item) => total + item.qty, 0);
 
   return (
     <div className="headerMainBg">
